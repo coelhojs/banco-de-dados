c940a6a3368e41fc06f964775e905aec11c9d383
Henrique Freire <h.freire@live.com>
2019-11-16 18:25:06 -0300

    Adiconando novos indexes

--- CREATE INDEX.sql
+++ CREATE INDEX.sql
@@ -2,3 +2,7 @@
 -- Esse índice permite saber quais bairros possuem ocorrências
 DROP INDEX IF EXISTS aviso_bairro_IDX ON AVISO;
 CREATE INDEX aviso_bairro_IDX ON AVISO (bairro);
+
+-- Esse índice facilita as pesquisas na Tabela Usuários
+ALTER TABLE `usuario`
+	ADD INDEX `campos_mais_usados_IDX` (`cpf`, `cidade`);
