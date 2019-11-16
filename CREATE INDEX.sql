7989f5816c13b0492965c8eb2681e08666b060c6
coelhojs <ccoelho.me@gmail.com>
2019-11-15 21:39:55 -0300

    novo index

--- CREATE INDEX.sql
+++ CREATE INDEX.sql
@@ -1,4 +1,4 @@
 -- Índice que retorna os bairros que possuem avisos cadastrados
 -- Esse índice permite saber quais bairros possuem ocorrências
-DROP INDEX IF EXISTS IX_bairros ON AVISO;
-CREATE INDEX IX_bairros ON AVISO (bairro);
+DROP INDEX IF EXISTS aviso_bairro_IDX ON AVISO;
+CREATE INDEX aviso_bairro_IDX ON AVISO (bairro);
