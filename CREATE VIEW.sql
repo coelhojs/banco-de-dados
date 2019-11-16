efb914e766ea36c951de1d7a1d9e382c6396ce05
Henrique Freire <h.freire@live.com>
2019-11-16 18:33:24 -0300

    Delete CREATE VIEW.sql

--- CREATE VIEW.sql
+++
@@ -1,5 +0,0 @@
--- View com os usuários de Contagem e com apenas campos selecionados
-CREATE VIEW
-usuarios_contagem 
-AS 
-SELECT nome, cpf, telefone, email FROM usuario WHERE Lower(cidade) = 'contagem' 
